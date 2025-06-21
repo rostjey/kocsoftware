71e40c18f7aab0b547f64e617e8fe3e963a6b7d2
Kaan Koç <kaankoc@Kaan-MacBook-Air.local>
2025-06-21 16:24:28 +0300

    yerak

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["res.cloudinary.com", "kocsoftware.onrender.com"],
+  },
 };
 
 export default nextConfig;
