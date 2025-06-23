d538355f3c76c118a6ce27d80fac25a6793cbc09
Kaan Koç <kaankoc@Kaan-MacBook-Air.local>
2025-06-23 18:03:44 +0300

    cafeclient güncelleme mjs js e döndü 5

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,31 +1,32 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-    content: [
-      "./app/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          retrored: "#751813",
-          retrohover: "#a42a1e",
-          retrotext: "#f1c40f",
-        },
-        animation: {
-          pulseShadow: 'pulseShadow 3s ease-in-out infinite',
-        },
-        keyframes: {
-          pulseShadow: {
-            '0%, 100%': {
-              boxShadow: '0 0 0px rgba(255, 165, 0, 0)',
-            },
-            '50%': {
-              boxShadow: '0 0 25px rgba(255, 165, 0, 0.5)',
-            },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        pulseShadow: 'pulseShadow 3s ease-in-out infinite',
+      },
+      keyframes: {
+        pulseShadow: {
+          '0%, 100%': {
+            boxShadow: '0 0 0px rgba(237, 187, 114, 0)',  //rgba(255, 165, 0, 0)
+          },
+          '50%': {
+            boxShadow: '0 0 25px rgba(237, 187, 114, 0.5)',
           },
         },
       },
+      colors: {
+        retrored: "#751813",
+        retrohover: "#a42a1e",
+        retrotext: "#edbb72",
+      },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+}
