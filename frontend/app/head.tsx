89cff84a80b9f4bacb45b5d2489d8bddd19d585f
Kaan Koç <kaankoc@Kaan-MacBook-Air.local>
2025-06-23 15:00:27 +0300

    head.tsx

---
+++ frontend/app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+    return (
+      <>
+        <title>kocsoftware.net</title>
+        <meta name="description" content="Koc Software - Cafe Yönetim Sistemi" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="theme-color" content="#ffffff" />
+      </>
+    );
+  }
+  
