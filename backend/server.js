ff9ee35bd5829d722b2134bda818c405e2a59ede
Kaan Koç <kaankoc@Kaan-MacBook-Air.local>
2025-06-23 04:10:59 +0300

    www için de domain ekledik

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,22 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL,   //|| "http://localhost:3000", // Frontend URL'sini buraya ekleyin
+const allowedOrigins = [
+    process.env.FRONTEND_URL,
+    process.env.FRONTEND_URL_WWW
+  ];
+  
+  app.use(cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS hatası: Bu origin izinli değil."));
+      }
+    },
     credentials: true
-}));
+  }));
+
 app.use(express.json());
 app.use(cookieParser());
 
