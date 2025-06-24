dd8385d8621ff02bea89e6b3c9ad6fabc240556e
Kaan Koç <kaankoc@Kaan-MacBook-Air.local>
2025-06-24 15:44:57 +0300

    head içine metadata dynamic ısland için

--- frontend/app/head.tsx
+++ frontend/app/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
       <>
         <title>kocsoftware.net</title>
         <meta name="description" content="Koc Software - Cafe Yönetim Sistemi" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
