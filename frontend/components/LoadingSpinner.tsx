6a238413431854ead557add0decb0958a85e97a2
Kaan Koç <kaankoc@Kaan-MacBook-Air.local>
2025-06-24 16:21:17 +0300

    yükleniyor eklendi renk mor

--- frontend/components/LoadingSpinner.tsx
+++ frontend/components/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 export default function LoadingSpinner() {
     return (
       <div className="flex justify-center items-center">
-        <div className="w-8 h-8 border-4 border-white border-t-transparent rounded-full animate-spin" />
+        <div className="w-8 h-8 border-4 border-purple-500 border-t-transparent rounded-full animate-spin" />
       </div>
     );
   }
