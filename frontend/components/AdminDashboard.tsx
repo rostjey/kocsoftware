74f7f7325d338da52310aeec15af5fd34cbaaa82
Kaan Koç <kaankoc@Kaan-MacBook-Air.local>
2025-06-25 22:15:16 +0300

    pp use state de tanımlanmamış

--- frontend/components/AdminDashboard.tsx
+++ frontend/components/AdminDashboard.tsx
@@ -21,6 +21,7 @@ export default function AdminDashboard({
     name: "",
     logo: "",
     instagram: "",
+    avatar: "",
     template: "scroll",
   });
   const [products, setProducts] = useState<Product[]>([]);
@@ -39,6 +40,7 @@ export default function AdminDashboard({
         name: cafeData.name || "",
         logo: cafeData.logo || "",
         instagram: cafeData.instagram || "",
+        avatar: cafeData.avatar || "", // avatar eklenmişse kullan
         template: cafeData.template || "scroll", //template: cafeData.template !== undefined ? cafeData.template : "scroll" //burası önemli en son seçilen kategorinin logout sonrası gözükmesi için
       });
 
